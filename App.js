6f627b03817596ad65a749c2bda447b9ca1d6f48
João Lucas <joao.marques115@aluno.ce.gov.br>
2026-02-24 22:01:54 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,44 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={ require("./unnamed.png")}  
+      />
+      <Text style={styles.texto}>Calculadora de IMC</Text>
+      <TextInput 
+        style={styles.TextInput}
+        placeholder='digite sua altura'
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+  logo: {
+    width: 300,
+    height: 300,
+  },
+  texto: {
+    fontSize: 30,
+  },
+  TextInput:{
+    borderColor: '#bdbdbd',
+    borderWidth: 1,
+    borderRadius: 100,
+    fontFamily: 'Times New Roman',
+    padding: 10,
+    
+  }
+});
